55012fcbd3e50f4a9b94a6e881e9be5eb420a4cf
mayurkashyap <mayurkashyap60@gmail.com>
2020-12-23 16:19:50 +0530

    add user session

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation')
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -15,9 +16,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New User Connected.');
 
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat App'));
 
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'));
+
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      callback('Name and room name are required.');
+    }
+
+    socket.join(params.room);
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat App'));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
+    callback();
+  });
 
   socket.on('createMessage', (createMsg, callback) => {
     console.log('Create Message', createMsg);
---
+++ server/utils/users.test.js
@@ -0,0 +1,18 @@
+const expect = require('expect');
+
+const {Users} = require('./users');
+
+describe('Users', () => {  
+  it('should add new users', () => {
+    var users = new Users();
+    var user = {
+      id: '123',
+      name: 'John',
+      room: 'The Office room'
+    };
+
+    var resUser = users.addUser(user.id, user.name, user.room);
+
+    expect(users.users).toEqual([user]);
+  });
+});
